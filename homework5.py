96efc570bb061422b252a39a25d0c3fe9722b403
Victor <fanflajok@gmail.com>
2024-07-21 01:19:41 +0300

    Add files via upload
    Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = tuple([4,8,15,16,23,42, ["enter"]])
+print(immutable_var)
+immutable_var [6][0]=str("108 min")
+print(immutable_var) 
+mutable_list = ["варёная колбаса", "яйца", "картофель", "огурец", "редис", "зелёный лук", "укроп", "соль", "кефир"]
+print(mutable_list)
+mutable_list[8]="квас"
+print(mutable_list)
